66a265fa62078a08698b3c03a7013029519361c5
MovementGH <48330137+MovementGH@users.noreply.github.com>
2020-03-04 17:05:47 -0500

    Test

--- src/sharding/Shard.js
+++ src/sharding/Shard.js
@@ -114,8 +114,8 @@ class Shard extends EventEmitter {
       this.process = childProcess
         .fork(path.resolve(this.manager.file), this.args, {
           env: this.env,
+          silent: true,
           execArgv: this.execArgv,
-          silent: true
         })
         .on('message', this._handleMessage.bind(this))
         .on('exit', this._exitListener);
